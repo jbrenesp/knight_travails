2583f5c395e6e1103d47897a1985b371520852b5
jbrenesp <josembrenes@gmail.com>
2026-01-28 13:34:31 +0200

    add boundaries and visited filters to BFS knight moves

--- knight.rb
+++ knight.rb
@@ -15,13 +15,14 @@ def knight_moves(start, target)
     current, path = queue.shift
     x, y = current
 
-    #if this position is the target, return the path
+    
 
     #generate all valid knight moves from this position
-    #skip moves that go off the board
-    #skip moves that have already been visited
+    
+    
     #mark new moves as visited
     #add new position and updated path to the queue
+    #if this position is the target, return the path
     if current == target
       return path
     end
@@ -29,6 +30,13 @@ def knight_moves(start, target)
     possible_moves.each do |dx, dy|
       new_x = x + dx
       new_y = y + dy
+      #skip moves that go off the board
+      next unless
+      new_x.between?(0,7) && new_y.between?(0,7)
+      #skip moves that have already been visited
+      next if visited.include?([new_x, new_y])
     end
+    
+    
   end
 end
