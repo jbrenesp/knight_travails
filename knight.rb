f5718abeb327bd5a8f4287d998db0ec83c3ddde7
jbrenesp <josembrenes@gmail.com>
2026-02-06 12:47:21 +0200

    Implement Knight Travails BFS solution with shortest-path tracking

--- knight.rb
+++ knight.rb
@@ -24,8 +24,12 @@ def knight_moves(start, target)
     #add new position and updated path to the queue
     #if this position is the target, return the path
     if current == target
+      puts "You made it in #{path.length-1} moves. Here's your path:"
+      path.each { |square| p square }
       return path
     end
+
+    x, y = current
 
     possible_moves.each do |dx, dy|
       new_x = x + dx
@@ -35,8 +39,13 @@ def knight_moves(start, target)
       new_x.between?(0,7) && new_y.between?(0,7)
       #skip moves that have already been visited
       next if visited.include?([new_x, new_y])
+      
+      new_position = [new_x, new_y]
+      visited << new_position
+      new_path = path + [new_position]
+      queue << [new_position, new_path]
     end
-    
-    
   end
 end
+
+knight_moves([3,3], [4,3])
